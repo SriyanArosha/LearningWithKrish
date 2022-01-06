c49f5cbd963b2442b77f8dbff301995ac9043f6a
unknown <sriyanarosha@gmail.com>
2022-01-06 17:48:15 +0530

    Networking Related Screenshots Added

---
+++ Microservices Practicals/Practicals 1-3/student-app/src/main/java/com/arosha/studentapp/Stage.java
@@ -0,0 +1,17 @@
+package com.arosha.studentapp;
+
+public class Stage {
+    int value;
+
+    public Stage(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
---
+++ Microservices Practicals/Practicals 1-3/student-app/src/main/java/com/arosha/studentapp/model/Student.java
@@ -0,0 +1,7 @@
+package com.arosha.studentapp.model;
+
+public class Student {
+    private int id;
+    private String fullName;
+    private int age;
+}
